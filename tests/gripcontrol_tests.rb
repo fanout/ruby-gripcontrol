bbfe54ade530e02ad270eaf51690ca69ebbb4654
Kon <bokarius@comcast.net>
2015-03-14 17:57:15 -0700

    added tests to gripcontrol

--- tests/gripcontrol_tests.rb
+++ tests/gripcontrol_tests.rb
@@ -110,4 +110,115 @@ class TestGripControl < Minitest::Test
     assert_equal(hold['response'], nil)
     assert_equal(hold['hold']['mode'], 'stream')
   end
+
+  def test_decode_websocket_events
+    events = GripControl.decode_websocket_events("OPEN\r\nTEXT 5\r\nHello" + 
+        "\r\nTEXT 0\r\n\r\nCLOSE\r\nTEXT\r\nCLOSE\r\n")
+    assert_equal(events.length, 6)
+    assert_equal(events[0].type, 'OPEN')
+    assert_equal(events[0].content, nil)
+    assert_equal(events[1].type, 'TEXT')
+    assert_equal(events[1].content, 'Hello')
+    assert_equal(events[2].type, 'TEXT')
+    assert_equal(events[2].content, '')
+    assert_equal(events[3].type, 'CLOSE')
+    assert_equal(events[3].content, nil)
+    assert_equal(events[4].type, 'TEXT')
+    assert_equal(events[4].content, nil)
+    assert_equal(events[5].type, 'CLOSE')
+    assert_equal(events[5].content, nil)
+    events = GripControl.decode_websocket_events("OPEN\r\n")
+    assert_equal(events.length, 1)
+    assert_equal(events[0].type, 'OPEN')
+    assert_equal(events[0].content, nil)
+    events = GripControl.decode_websocket_events("TEXT 5\r\nHello\r\n")
+    assert_equal(events.length, 1)
+    assert_equal(events[0].type, 'TEXT')
+    assert_equal(events[0].content, 'Hello')
+    assert_raises RuntimeError do 
+      GripControl.decode_websocket_events("TEXT 5")
+    end
+    assert_raises RuntimeError do 
+      GripControl.decode_websocket_events("OPEN\r\nTEXT")
+    end
+  end
+
+  def test_encode_websocket_events  
+    events = GripControl.encode_websocket_events([
+        WebSocketEvent.new("TEXT", "Hello"), 
+        WebSocketEvent.new("TEXT", ""),
+        WebSocketEvent.new("TEXT", nil)])
+    assert_equal(events, "TEXT 5\r\nHello\r\nTEXT 0\r\n\r\nTEXT\r\n")
+    events = GripControl.encode_websocket_events([WebSocketEvent.new("OPEN")])
+    assert_equal(events, "OPEN\r\n")
+  end
+
+  def test_websocket_control_message
+    message = GripControl.websocket_control_message('type')
+    assert_equal(message, '{"type":"type"}')
+    message = JSON.parse(GripControl.websocket_control_message('type', {'arg1' => 'val1',
+        'arg2' => 'val2'}))
+    assert_equal(message['type'], 'type')
+    assert_equal(message['arg1'], 'val1')
+    assert_equal(message['arg2'], 'val2')
+  end
+
+  def test_parse_channels
+    channels = GripControl.parse_channels('channel')
+    assert_equal(channels[0].name, 'channel')
+    assert_equal(channels[0].prev_id, nil)
+    channels = GripControl.parse_channels(Channel.new('channel'))
+    assert_equal(channels[0].name, 'channel')
+    assert_equal(channels[0].prev_id, nil)
+    channels = GripControl.parse_channels(Channel.new('channel', 'prev-id'))
+    assert_equal(channels[0].name, 'channel')
+    assert_equal(channels[0].prev_id, 'prev-id')
+    channels = GripControl.parse_channels([Channel.new('channel1', 'prev-id'),
+        Channel.new('channel2')])
+    assert_equal(channels[0].name, 'channel1')
+    assert_equal(channels[0].prev_id, 'prev-id')
+    assert_equal(channels[1].name, 'channel2')
+    assert_equal(channels[1].prev_id, nil)
+    assert_raises RuntimeError do 
+      GripControl.parse_channels([])
+    end
+  end
+
+  def test_get_hold_channels
+    hold_channels = GripControl.get_hold_channels([Channel.new('channel')])
+    assert_equal(hold_channels[0], {'name' => 'channel'})
+    hold_channels = GripControl.get_hold_channels([
+        Channel.new('channel', 'prev-id')])
+    assert_equal(hold_channels[0], {'name' => 'channel', 'prev-id' =>
+        'prev-id'})
+    hold_channels = GripControl.get_hold_channels([
+        Channel.new('channel1', 'prev-id1'), Channel.new('channel2', 'prev-id2')])
+    assert_equal(hold_channels[0], {'name' => 'channel1', 'prev-id' =>
+        'prev-id1'})
+    assert_equal(hold_channels[1], {'name' => 'channel2', 'prev-id' =>
+        'prev-id2'})
+  end
+
+  def test_get_hold_response
+    response = GripControl.get_hold_response(nil)
+    assert_equal(response, nil)
+    response = GripControl.get_hold_response('body')
+    assert_equal(response['body'], 'body')
+    assert_equal(response.key?('code'), false)
+    assert_equal(response.key?('reason'), false)
+    assert_equal(response.key?('headers'), false)
+    response = GripControl.get_hold_response('body'.force_encoding('ASCII-8BIT'))
+    assert_equal(response['body-bin'], Base64.encode64('body'))
+    response = GripControl.get_hold_response(Response.new('code', 'reason',
+        {'header1' => 'val1'}, 'body'))
+    assert_equal(response['code'], 'code')
+    assert_equal(response['reason'], 'reason')
+    assert_equal(response['headers'], {'header1' => 'val1'})
+    assert_equal(response['body'], 'body')
+    response = GripControl.get_hold_response(Response.new(nil, nil, {}, nil))
+    assert_equal(response.key?('headers'), false)
+    assert_equal(response.key?('body'), false)
+    assert_equal(response.key?('reason'), false)
+    assert_equal(response.key?('headers'), false)
+  end
 end
